925b29d4e1397d6dc819b2da043e0babfc370a22
Adarnok <Adarnok@poczta.fm>
2018-08-22 23:30:57 +0200

    Pierwszy commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,37 @@
 'use strict';
-function calc(b) {
-    const res = document.querySelector('#res');
-    const liczba = b.value;
-    if (b.value == "=") {
-        res.value = eval(res.value);
+(function(){
+    
+    function Kalkulator(result){
+        this._guziczki = document.querySelectorAll('input[type="button"]');
+        this._resPlace = document.querySelector('#' + result);
+        console.log(this._guziczki);
+        console.log(this._resultPlace);
+        this._guziki(this._guziczki);
     }
-    else
-    if (b) {
-        res.value += liczba;
-    }   
-    if (b.value == "C") {
-        res.value = '';
+
+
+    // Kalkulator.prototype._guziki = function(guzik){
+    //     guzik.forEach(function(elem, i){
+    //         elem.addEventListener('click', this._calc.bind(this), this);
+    //     })
+    // }
+    Kalkulator.prototype._guziki = function(guzik){
+        guzik.forEach(this._click, this);
     }
-}
+    Kalkulator.prototype._click = function(elem, i){
+        elem.addEventListener('click', this._calc.bind(this));
+    }
+    
+    Kalkulator.prototype._calc = function(button){
+        const buttonValue = button.target.value;
+        if(buttonValue === 'C'){
+            this._resPlace.value = '';
+        }else if(buttonValue ==='='){
+            this._resPlace.value = eval(this._resPlace.value);
+        }else {
+            this._resPlace.value += buttonValue;
+        }
+    }
+    new Kalkulator('res');
+    // new Kalkulator();
+})()
